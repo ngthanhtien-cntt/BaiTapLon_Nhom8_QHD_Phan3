5c27e995b82932111c5456633ab761a02b6a7b4b
Nguyen Thanh Tien <tiennguyen59382@gmail.com>
2024-08-08 20:17:49 +0700

    phan 3 giai thuat QHD

---
+++ TimDuongDiToiUu/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace BTL_Nhom8_Phan3
+{
+    internal class QuiHoachDong_TimDuongDiToiUu
+    {
+        public void TimDuongDiToiUu(int[,] A)
+        {
+            int m = A.GetLength(0);
+            int n = A.GetLength(1);
+
+
+            int[,] B = new int[m, n];
+
+
+            for (int i = 0; i < m; i++)
+            {
+                B[i, 0] = A[i, 0];
+            }
+
+            for (int j = 1; j < n; j++)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    int maxPrev = B[i, j - 1];
+                    if (i > 0)
+                    {
+                        maxPrev = Math.Max(maxPrev, B[i - 1, j - 1]);
+                    }
+                    if (i < m - 1)
+                    {
+                        maxPrev = Math.Max(maxPrev, B[i + 1, j - 1]);
+                    }
+                    B[i, j] = A[i, j] + maxPrev;
+                }
+            }
+
+
+            int maxPoints = B[0, n - 1];
+            int startRow = 0;
+            for (int i = 1; i < m; i++)
+            {
+                if (B[i, n - 1] > maxPoints)
+                {
+                    maxPoints = B[i, n - 1];
+                    startRow = i;
+                }
+            }
+
+            int[] path = new int[n];
+            path[n - 1] = startRow;
+            for (int j = n - 2; j >= 0; j--)
+            {
+                int i = path[j + 1];
+                if (i > 0 && B[i, j + 1] == B[i - 1, j] + A[i, j + 1])
+                {
+                    path[j] = i - 1;
+                }
+                else if (i < m - 1 && B[i, j + 1] == B[i + 1, j] + A[i, j + 1])
+                {
+                    path[j] = i + 1;
+                }
+                else
+                {
+                    path[j] = i;
+                }
+            }
+
+
+            Console.WriteLine("Điem so lon nhat co the dat duoc : " + maxPoints);
+            Console.Write("Đuong di : ");
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"({path[j] + 1}, {j + 1}) ");
+            }
+            Console.WriteLine();
+        }
+        static void Main()
+        {
+            int[,] A = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+            QuiHoachDong_TimDuongDiToiUu qhd = new QuiHoachDong_TimDuongDiToiUu();
+            qhd.TimDuongDiToiUu(A);
+        }
+    }
+}
---
+++ XauConChungDaiNhat/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BTL_Nhom8_Phan3
+{
+    internal class QHD_XauConChungDaiNhat
+    {
+
+        public static int TinhDoDaiXauConChungDaiNhat(string S1, string S2)
+        {
+            int lengthS1 = S1.Length;
+            int lengthS2 = S2.Length;
+            int[,] dp = new int[lengthS1 + 1, lengthS2 + 1];
+            int maxLength = 0;
+
+            for (int i = 1; i <= lengthS1; i++)
+            {
+                for (int j = 1; j <= lengthS2; j++)
+                {
+                    if (S1[i - 1] == S2[j - 1])
+                    {
+                        dp[i, j] = dp[i - 1, j - 1] + 1;
+                        if (dp[i, j] > maxLength)
+                        {
+                            maxLength = dp[i, j];
+                        }
+                    }
+                    else
+                    {
+                        dp[i, j] = 0;
+                    }
+                }
+            }
+
+            return maxLength;
+        }
+
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Nhap xau  S1:");
+            string S1 = Console.ReadLine();
+            Console.WriteLine("Nhap xau S2:");
+            string S2 = Console.ReadLine();
+
+            int maxLength = TinhDoDaiXauConChungDaiNhat(S1, S2);
+
+
+            Console.WriteLine($"Đo dai xau  con chung dai nhat: {maxLength}");
+        }
+    }
+}
